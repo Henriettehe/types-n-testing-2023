de9db68699866f886f0c8f2a37d12d4fca354353
HenrietteH <Henriettehestsveen@gmail.com>
2023-09-05 18:30:41 +0200

    npm install

--- 0-tasks/src/name.js
+++ 0-tasks/src/name.js
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
-export function splitName(fullName) {
-  const parts = fullName.split(' ')
-  return {
-    firstName: parts[0],
-    lastName: parts[1],
-  }
-}
-=======
 /* export function splitName(fullName) {
   // TODO implement to pass the test
   const parts = fullName.split('') 
@@ -15,4 +6,3 @@ export function splitName(fullName) {
       lastName: parts[1],
     }
   } */
->>>>>>> Stashed changes
