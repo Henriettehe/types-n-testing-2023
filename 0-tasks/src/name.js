57323da3e6288cb81cc34cf5fcdfb6ed4d3edd70
HenrietteH <Henriettehestsveen@gmail.com>
2023-09-05 18:48:38 +0200

    Startet npm install og run test 😄

--- 0-tasks/src/name.js
+++
@@ -1,8 +0,0 @@
-/* export function splitName(fullName) {
-  // TODO implement to pass the test
-  const parts = fullName.split('') 
-    return {
-      firstName: parts[0],
-      lastName: parts[1],
-    }
-  } */
--- 0-tasks/src/name.test.js
+++
@@ -1,9 +0,0 @@
-/* import { splitName } from './name'
-
-it('should return object with firstName and lastName', () => {
-  const fullName = 'John Doe'
-  const result = splitName(fullName)
-
-  expect(result).toEqual({ firstName: 'John', lastName: 'Doe' })
-})
- */
